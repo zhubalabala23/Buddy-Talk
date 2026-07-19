5e702d205151f0b1973b9600f533cfac343456c8
zhubalabala23 <raka230803@gmail.com>
2026-07-19 16:14:10 +0700

    add .ENV for hosting to vercell

--- src/firebase.js
+++ src/firebase.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD5RbahWpzI6X_30hqZ9BHw7LfwxVy4fwA",
-  authDomain: "buddy-talk-1955c.firebaseapp.com",
-  projectId: "buddy-talk-1955c",
-  storageBucket: "buddy-talk-1955c.firebasestorage.app",
-  messagingSenderId: "299535253970",
-  appId: "1:299535253970:web:3cbb248b877bf7be3d480b"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY || "AIzaSyD5RbahWpzI6X_30hqZ9BHw7LfwxVy4fwA",
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN || "buddy-talk-1955c.firebaseapp.com",
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID || "buddy-talk-1955c",
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET || "buddy-talk-1955c.firebasestorage.app",
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID || "299535253970",
+  appId: import.meta.env.VITE_FIREBASE_APP_ID || "1:299535253970:web:3cbb248b877bf7be3d480b"
 };
 
 const app = initializeApp(firebaseConfig);
